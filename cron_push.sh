6b2b5e318b4474370e898ca324de16a755e5e5ab
10ma <stric10ma@gmail.com>
2019-01-23 14:52:10 +0900

    fix: 余計なpushを排除

--- cron_push.sh
+++ cron_push.sh
@@ -9,7 +9,6 @@ git branch |grep -q $(date +%Y%m) || git checkout -b $(date +%Y%m)
 commit_push(){
   git add -A
   git commit -m "$(date +%d_%H:%M)"
-  git push origin $(date +%Y%m%d)
   git push origin $(date +%Y%m) 2>&1 >> /var/log/git-push.log
 }
 
